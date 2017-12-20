3ad732cb75ad4f0992244d89f98c252e617f4524
Scott Laufer <slaufer@synacor.com>
2017-12-20 15:27:27 -0500

    Optimizations to vault helper script

--- misc/vault.py
+++ misc/vault.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-import curses;
+from time import time
+import operator
 
-def evalpath(vault, path):
+def path_weight(vault, path):
 	(x, y) = path[0]
-	value = vault[y][x]
-	opstring = ""
+	weight = vault[y][x]
 
-	for (x, y) in path[1:]:
-		if isinstance(vault[y][x], int):
-			value = eval(str(value) + opstring + str(vault[y][x]))
-		else:
-			opstring = vault[y][x]
+	for i in range(2, len(path), 2):
+		(x, y) = path[i]
+		(ox, oy) = path[i-1]
+		weight = vault[oy][ox](weight, vault[y][x])
 
-	return value
+	return weight
 
-def pathtext(path):
+def path_string(path):
 	text = ""
 	last = path[0]
 
@@ -35,70 +34,67 @@ def pathtext(path):
 
 	return text
 
-def step(vault, travel, start, end, maxdepth, goal, path=None, depth=None, paths=None):
-	# if the path isn't initialized, do so now
-	if None in [path, depth, paths]:
-		path = [start]
-		paths = []
-		depth = 1
+def step(vault, travel, start, end, maxdepth, goal):
+	paths = []
+	stack = [[start]]
+	count = 0
 
-	# if we've gone too deep, return
-	if depth > maxdepth:
-		return paths
+	while stack:
+		count += 1
+		path = stack.pop()
+		pos = path[-1]
+		(x, y) = pos
 
-	# figure out what square we're on
-	(x, y) = path[-1]
+		# paths that return to the antechamber fail
+		if pos == start and len(path) > 1:
+			continue
 
+		# if this is a numeric literal room, check the status of the path
+		if isinstance(vault[y][x], int):
+			# get path weight
+			weight = path_weight(vault, path)
 
-	# if we're on a literal square, determine if we need to do anything
-	if isinstance(vault[y][x], int):
-		# if we went back into the antechamber, instant failure
-		if (x, y) == start and depth > 1:
-			return paths
+			# paths with negative weights fail
+			if weight < 0:
+				continue
 
-		# get the path weight so far
-		weight = evalpath(vault, path)
-	
-		# negative weights are an instant failure
-		if weight < 0:
-			return paths
+			# if we're in the orb room, the path is over one way or another
+			if pos == end:
+				# if this path matches our criteria, save it
+				if weight == goal:
+					paths.append(path)
 
-		# if we're in the vault door chamber
-		if (x, y) == end:
-			# weight == 30 is a success, anything else is a failure
-			if weight == goal:
-				paths.append(path[:])
+				continue
 
-			# success or failure, entering the orb chamber is the end of the path
-			return paths
+		# if this is the deepest we can go, move on
+		if len(path) >= maxdepth:
+			continue
 
-	# traverse into all valid adjacent squares
-	for target in [(x+dx, y+dy) for (dx, dy) in travel if x+dx in range(0, len(vault)) and y+dy in range(0, len(vault[0]))]:
-		path.append(target)
-		step(vault, travel, start, end, maxdepth, goal, path, depth + 1, paths)
-		path.pop()
+		# if this path hasn't been cut short yet, queue its children
+		stack += [path + [(x+dx, y+dy)] for (dx, dy) in travel if x+dx in range(0, len(vault)) and y+dy in range(0, len(vault[0]))]
 
 	return paths
 
 vault = [
-	[ '*', 8  , '-', 1   ],
-	[ 4  , '*', 11 , '*' ],
-	[ '+', 4  , '-', 18  ],
-	[ 22 , '-', 9  , '*' ]
+	[ operator.mul, 8  , operator.sub, 1   ],
+	[ 4  , operator.mul, 11 , operator.mul ],
+	[ operator.add, 4  , operator.sub, 18  ],
+	[ 22 , operator.sub, 9  , operator.mul ]
 ];
 travel = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 start = (0, 3)
 end = (3, 0)
-maxdepth = 30
+maxdepth = 15
 goal = 30
 
-for i in range(1, maxdepth + 1):
-	print("Searching for paths with %d steps or less..." % i)
+for i in range(1, maxdepth + 1, 2):
+	print('*** Looking for solutions with up to {:n} steps, approx. {:n} nodes to visit'.format(i, sum([j**4 for j in range(1, i+1, 2)])))
 
+	begin_time = time()
 	paths = step(vault, travel, start, end, i, goal)
+	elapsed_time = time() - begin_time
+	print('*** Finished in {:n} seconds'.format(elapsed_time))
 
 	for path in paths:
-		print("%d steps: %s\n" % (len(path), pathtext(path)))
+		print('{:n}: {:s}'.format(len(path), path_string(path)))
 
-	if paths:
-		break
