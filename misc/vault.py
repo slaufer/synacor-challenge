6bb34e14ee8d9546c8ecca4672e3bc0898bd2a59
Scott Laufer <slaufer@synacor.com>
2017-12-20 15:54:59 -0500

    More vault helper updates

--- misc/vault.py
+++ misc/vault.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from time import time
-import operator
+from operator import mul, sub, add
 
+# path_weight - finds the weight on a path
+# vault: vault through which to traverse
+# path:  path to follow
 def path_weight(vault, path):
 	(x, y) = path[0]
 	weight = vault[y][x]
@@ -13,6 +16,8 @@ def path_weight(vault, path):
 
 	return weight
 
+# path_string - converts a list of path tuples to a string of north/south/east/west directions
+# path: list of (x, y) tuples
 def path_string(path):
 	text = ""
 	last = path[0]
@@ -34,13 +39,22 @@ def path_string(path):
 
 	return text
 
+# step - solves the part 8 maze
+# vault:    2D (y: north->south; x: east->west) array of vault rooms; numeric literal for number rooms,
+#           operator.<op> for operator rooms
+# travel:   legal travel directions as array of (x, y) tuples
+#           (e.g. [(0, -1), (0, 1)] would only allow north/south travel)
+# start:    (x, y) tuple of starting position
+# end:      (x, y) tuple of end position
+# maxdepth: maximum number of moves to make
+# goal:     desired value at end position
 def step(vault, travel, start, end, maxdepth, goal):
 	paths = []
 	stack = [[start]]
-	count = 0
+	visited = 0
 
 	while stack:
-		count += 1
+		visited += 1
 		path = stack.pop()
 		pos = path[-1]
 		(x, y) = pos
@@ -49,8 +63,8 @@ def step(vault, travel, start, end, maxdepth, goal):
 		if pos == start and len(path) > 1:
 			continue
 
-		# if this is a numeric literal room, check the status of the path
-		if isinstance(vault[y][x], int):
+		# odd path lengths should always put you in a numeric literal room, so check the status of the path
+		if len(path) & 1 == 1:
 			# get path weight
 			weight = path_weight(vault, path)
 
@@ -73,13 +87,13 @@ def step(vault, travel, start, end, maxdepth, goal):
 		# if this path hasn't been cut short yet, queue its children
 		stack += [path + [(x+dx, y+dy)] for (dx, dy) in travel if x+dx in range(0, len(vault)) and y+dy in range(0, len(vault[0]))]
 
-	return paths
+	return (visited, paths)
 
 vault = [
-	[ operator.mul, 8  , operator.sub, 1   ],
-	[ 4  , operator.mul, 11 , operator.mul ],
-	[ operator.add, 4  , operator.sub, 18  ],
-	[ 22 , operator.sub, 9  , operator.mul ]
+	[ mul, 8  , sub, 1   ],
+	[ 4  , mul, 11 , mul ],
+	[ add, 4  , sub, 18  ],
+	[ 22 , sub, 9  , mul ]
 ];
 travel = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 start = (0, 3)
@@ -88,12 +102,12 @@ maxdepth = 15
 goal = 30
 
 for i in range(1, maxdepth + 1, 2):
-	print('*** Looking for solutions with up to {:n} steps, approx. {:n} nodes to visit'.format(i, sum([j**4 for j in range(1, i+1, 2)])))
+	print('##### Scanning up to {:n} steps #####'.format(i))
 
-	begin_time = time()
-	paths = step(vault, travel, start, end, i, goal)
-	elapsed_time = time() - begin_time
-	print('*** Finished in {:n} seconds'.format(elapsed_time))
+	begin = time()
+	(visited, paths) = step(vault, travel, start, end, i, goal)
+	elapsed = time() - begin
+	print('### {:n} nodes visited in {:n} seconds, {:n} results'.format(visited, elapsed, len(paths)))
 
 	for path in paths:
 		print('{:n}: {:s}'.format(len(path), path_string(path)))
