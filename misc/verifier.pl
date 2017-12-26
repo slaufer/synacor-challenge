8ec3b8c1a2a5b75ba3f6f194896f369556c4d2b9
Scott Laufer <slaufer@synacor.com>
2017-12-26 14:08:08 -0500

    Added timer to verifier

--- misc/verifier.pl
+++ misc/verifier.pl
@@ -34,6 +34,21 @@ sub verify {
 	return $arg0;
 }
 
-for (my $i = 1; $i < 10; $i++) {
-	printf "%d => %d\n", $i, verify(4, 1, $i, {});
+sub to_hms {
+	
+	my $secs = shift;
+	return sprintf("%02d:%02d:%02d", int($secs / 3600), int(($secs / 60) % 60), $secs % 60);
+}
+
+my $first = 0;
+my $count = 32768;
+
+my $start = time;
+for (my $i = $first; $i < $first + $count; $i++) {
+	my $done = ($i - $first + 1);
+	my $undone = $count - $done;
+	my $elapsed = time - $start;
+	my $remain = $elapsed / $done * $undone;
+
+	printf "%d => %d (%s elapsed, ETA %s)\n", $i, verify(4, 1, $i, {}), to_hms($elapsed), to_hms($remain);
 }
